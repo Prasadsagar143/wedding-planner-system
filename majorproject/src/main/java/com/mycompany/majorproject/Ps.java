693e79342645b90bf4e9a8987897316fab538fae
Sagar Hiralal Prasad <99202503+Prasadsagar143@users.noreply.github.com>
2023-06-30 19:01:32 +0530

    Add files via upload

---
+++ majorproject/src/main/java/com/mycompany/majorproject/Ps.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.majorproject;
+
+/**
+ *
+ * @author Sagar prasad
+ */
+class Ps {
+
+    static void executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setString(int i, String MobileNo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
